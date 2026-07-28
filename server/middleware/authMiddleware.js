71f667c59b6dc12e0d12be063fd24ac6a03598e0
yash jadhav <yashj6793@gmail.com>
2026-07-29 03:03:00 +0530

    Add session check to auth middleware

--- server/middleware/authMiddleware.js
+++ server/middleware/authMiddleware.js
@@ -1,6 +1,7 @@
 const jwt = require("jsonwebtoken");
+const User = require("../models/User");
 
-const protect = (req, res, next) => {
+const protect = async (req, res, next) => {
   try {
     const token = req.headers.authorization?.split(" ")[1];
 
@@ -9,8 +10,21 @@ const protect = (req, res, next) => {
     }
 
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    req.user = decoded;
 
+    const user = await User.findById(decoded.id).select("currentSessionId");
+
+    if (!user) {
+      return res.status(401).json({ message: "User not found" });
+    }
+
+    if (user.currentSessionId !== decoded.sessionId) {
+      return res.status(401).json({
+        message: "Logged in on another device",
+        code: "SESSION_INVALIDATED",
+      });
+    }
+
+    req.user = decoded;
     next();
   } catch (error) {
     res.status(401).json({ message: "Invalid token" });
